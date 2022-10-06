dff26c75ee312d06fe7466873c57934a9266e660
Joseph103 <joseph.103@hotmail.es>
2022-10-06 02:03:21 -0500

    my first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { environment } from '@src/environments/environment';
+import { test } from "@app/test";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'client-inmueble-app';
+}
